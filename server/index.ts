7cfbf2601bb95bdcabb65026801cc4bd7c64e38f
balinux <ryojuniyantara@gmail.com>
2025-10-28 06:49:00 +0800

    add: data fetching -  implement hono client on react and setup on server hono

--- server/index.ts
+++ server/index.ts
@@ -2,15 +2,16 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+const router = app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-.get('/api/users', (c) => {
-  return c.json([
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' },
-    { id: 3, name: 'Charlie' },
-  ])
-})
+  .get('/api/users', (c) => {
+    return c.json([
+      { id: 1, name: 'Alice' },
+      { id: 2, name: 'Bob' },
+      { id: 3, name: 'Charlie' },
+    ])
+  })
 
+export type AppType = typeof router
 export default app
